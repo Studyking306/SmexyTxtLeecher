175d1144595b7a9e317907967d4ec9aa1f2b06ad
Vip-00 <mrspect0011@gmail.com>
2024-10-06 19:20:49 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 #𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "25263708"
-api_hash = "9bde48b267ce65a576b478c0ff8c7bbb"
-bot_token = "7395475299:AAF4UK-zkoanFNX2s0p4UdsGXIQF5POr8gU"
+api_id = "18156248"
+api_hash = "db946fb6805b1a698c679626b617e77a"
+bot_token = "6511161031:AAFjShVa1YtVyO1liNpgG4lw5ftMC4T28LU"
 # Bot Username = "https://t.me/SmexyTxtLeechingBot"
