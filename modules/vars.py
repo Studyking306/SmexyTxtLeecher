d419bb94f66a061e2a8d2a6df90bcc077d0f131a
Studyking306 <akbartheking7860@gmail.com>
2024-10-14 23:47:57 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 #𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "18156248"
-api_hash = "db946fb6805b1a698c679626b617e77a"
-bot_token = "6511161031:AAFjShVa1YtVyO1liNpgG4lw5ftMC4T28LU"
-# Bot Username = "https://t.me/SmexyTxtLeechingBot"
+api_id = "22188044"
+api_hash = "099e3a1dce52b7677299c3ab8ab3b6ca"
+bot_token = "7718446072:AAFc9ke_wyu-Z6RZUrNXkX9olwW3LAySh20"
+# Bot Username = "http://t.me/Vsjsish_bot"
